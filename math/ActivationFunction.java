1c02ae229cf110940121d4c1b01425c067392ba7
jeromepl <jerome-pl2@hotmail.ca>
2017-01-28 11:05:56 -0500

    WIP: Added tanh and ReLU activation functions

--- math/ActivationFunction.java
+++ math/ActivationFunction.java
@@ -21,5 +21,42 @@ public abstract class ActivationFunction extends Function1d {
 		}
 	};
 	
+	public static final ActivationFunction TANH = new ActivationFunction() {
+		@Override
+		public double f(double x) {
+			return Math.tanh(x);
+		}
+		
+		@Override
+		public Function1d derivative() {
+			return new Function1d() {
+				@Override
+				public double f(double x) {
+					return 4 * Math.pow(Math.cosh(x), 2) / Math.pow(Math.cosh(2*x) + 1, 2); // sech(x)^2
+				}
+			};
+			
+		}
+	};
+	
+	
+	public static final ActivationFunction RELU = new ActivationFunction() {
+		@Override
+		public double f(double x) {
+			return (x > 0)? x: 0;
+		}
+		
+		@Override
+		public Function1d derivative() {
+			return new Function1d() {
+				@Override
+				public double f(double x) {
+					return (x > 0)? 1: 0;
+				}
+			};
+			
+		}
+	};
+	
 	public abstract Function1d derivative();
 }
