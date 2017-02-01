2f580961394e8467cd7e3ae87ca4cada6b86e67e
jeromepl <jerome-pl2@hotmail.ca>
2017-01-31 22:12:38 -0500

    Refactor: Moved layers to new branch

--- layers/Layer.java
+++
@@ -1,9 +0,0 @@
-package layers;
-
-import math.Matrix;
-
-public abstract class Layer {
-	
-	public abstract Matrix evaluate(Matrix input);
-	
-}
--- network_efficient/Layer.java
+++
@@ -1,9 +0,0 @@
-package network_efficient;
-
-public abstract class Layer {
-	
-	public abstract double[] evaluate(double[] input);
-	
-	public abstract double[] backPropagate(double[] output, double[] previousError);
-	
-}
